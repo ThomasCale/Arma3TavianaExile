c26ce33036a0ef7456d3d0247e50ceac203219bd
ThomasCale <chris_james_ansell@hotmail.com>
2020-04-09 05:21:38 +1000

    server push 3

---
+++ mpmissions/Exile.Taviana/TRADERS/ESP/ItemListESP.hpp
@@ -0,0 +1,169 @@
+	///////////////////////////////////////////////////////////////////////////////
+	// Drinks
+	///////////////////////////////////////////////////////////////////////////////
+
+	class DDR_Item_Franta                           { quality = 1; price = 44; };         //  Franta     
+	class DDR_Item_Redgull                          { quality = 1; price = 44; };         //  Red Gull   
+	class DDR_Item_Spirit                           { quality = 1; price = 44; };         //  Spirit     
+	class DDR_Item_Coke                             { quality = 1; price = 44; };         //  Coke       
+	class DDR_Item_Jackoff                          { quality = 1; price = 100; };         //  Jack Off's 
+	class DDR_Item_Yummy_Juice                      { quality = 1; price = 64; };         //  Yummy Juice
+
+	///////////////////////////////////////////////////////////////////////////////
+	// Food
+	///////////////////////////////////////////////////////////////////////////////
+
+	class DDR_Item_Bakedbeans                       { quality = 1; price = 34; };         //  Baked Beans (Raw)     
+	class DDR_Item_Bakedbeans_Cooked                { quality = 1; price = 34; };         //  Baked Beans (Cooked)  
+	class DDR_Item_Cereal_Crunchin                  { quality = 1; price = 34; };         //  Cereal (Crunchin Crisps)
+	class DDR_Item_Orange                           { quality = 1; price = 34; };         //  Orange                
+	class DDR_Item_Milkpowder                       { quality = 1; price = 34; };         //  Dry Milk Powder         
+	class DDR_Item_Pumpkin                          { quality = 1; price = 34; };         //  Pumpkin             
+	class DDR_Item_Rice                             { quality = 1; price = 34; };         //  Rice (Raw)            
+	class DDR_Item_Rice_Cooked                      { quality = 1; price = 34; };         //  Rice (Cooked)        
+	class DDR_Item_Tacticalbacon                    { quality = 1; price = 34; };         //  Tactical Bacon (Raw)    
+	class DDR_Item_Tacticalbacon_Cooked             { quality = 1; price = 34; };         //  Tactical Bacon (Cooked) 
+	class DDR_Item_Salema                           { quality = 1; price = 94; };         //  Salema Fish (Raw)      
+	class DDR_Item_Mackerel                         { quality = 1; price = 94; };         //  Mackerel Fish (Raw)     
+	class DDR_Item_Tunaa                            { quality = 1; price = 94; };         //  Tuna Fish (Raw)         
+	class DDR_Item_Mullet                           { quality = 1; price = 94; };         //  Mullet Fish (Raw)     
+	class DDR_Item_CatShark                         { quality = 1; price = 94; };         //  Cat Shark Fish (Raw)   
+	class DDR_Item_Ornate                           { quality = 1; price = 94; };         //  Ornate Fish (Raw)       
+	class DDR_Item_Turtle                           { quality = 1; price = 94; };         //  Turtle (Raw)          
+	class DDR_Item_Apple_Red                        { quality = 1; price = 34; };         //  A Juicy Red Apple    
+	class DDR_Item_Apple                            { quality = 1; price = 34; };         //  A Juicy Apple           
+	class DDR_Item_Apple_Green                      { quality = 1; price = 34; };         //  A Juicy Green Apple     
+	class DDR_Item_Blueberry                        { quality = 1; price = 34; };         //  Juicy Blue Berries      
+	class DDR_Item_Rottenberry                      { quality = 1; price = 34; };         //  Rotten Berries          
+	class DDR_Item_Redberry                         { quality = 1; price = 34; };         //  Juicy Red Berries      
+	class DDR_Item_Banana                           { quality = 1; price = 34; };         //  A Fat Banana           
+	class DDR_Item_Pear                             { quality = 1; price = 34; };         //  A Juicy Pear           
+	class DDR_Item_Tomato                           { quality = 1; price = 34; };         //  Tomato                  
+	class DDR_Item_Strawberry                       { quality = 1; price = 34; };         //  Juicy Strawberry        
+	class DDR_Item_Rotten_Orange                    { quality = 1; price = 34; };         //  Rotten Orange           
+	class DDR_Item_Rotten_Apple                     { quality = 1; price = 34; };         //  Rotten Apple            
+	class DDR_Item_Rotten_Apricot                   { quality = 1; price = 34; };         //  Rotten Apricot        
+	class DDR_Item_Hamburger                        { quality = 1; price = 34; };         //  Yummy Burger          
+	class DDR_Item_Potato                           { quality = 1; price = 34; };         //  A Fresh Potato          
+	class DDR_Item_Melon                            { quality = 1; price = 34; };         //  Fresh Watermelon        
+	class DDR_Item_Beet                             { quality = 1; price = 34; };         //  Fresh Beet            
+	class DDR_Item_Bread                            { quality = 1; price = 34; };         //  Fresh Bread           
+	class DDR_Item_Bread_Roll                       { quality = 1; price = 34; };         //  Fresh Bun              
+	class DDR_Item_Passion_Fruit                    { quality = 1; price = 34; };         //  Fresh Passion Fruit     
+	class DDR_Item_Pomegranate                      { quality = 1; price = 34; };         //  Fresh Pomegranate      
+	class DDR_Item_Rotten_Pear                      { quality = 1; price = 34; };         //  Rotten Pear             
+	class DDR_Item_Rotten_Melon                     { quality = 1; price = 34; };         //  Rotten Watermelon       
+	class DDR_Item_Rotten_Banana                    { quality = 1; price = 34; };         //  Rotten Banana           
+	class DDR_Item_Cucumber                         { quality = 1; price = 34; };         //  Zucchini                
+
+	///////////////////////////////////////////////////////////////////////////////
+	// Food (Container)
+	///////////////////////////////////////////////////////////////////////////////
+                                                                                                       
+	class DDR_Item_Container_Soup                   { quality = 1; price = 134; };        //  (Special Soup)          
+	class DDR_Item_Container_Chicken_Rice           { quality = 1; price = 134; };        //  (Chicken/Rice)          
+	class DDR_Item_Container_Alsatian_Rice 	        { quality = 1; price = 134; };        //  (Alsatian/Rice)         
+	class DDR_Item_Container_CatShark_Rice    	    { quality = 1; price = 134; };        //  (Cat Shark Filet/Rice)  
+	class DDR_Item_Container_Fin_Rice        	    { quality = 1; price = 134; };        //  (Fin/Rice)              
+	class DDR_Item_Container_Goat_Rice        	    { quality = 1; price = 134; };        //  (Goat/Rice)             
+	class DDR_Item_Container_Mackerel_Rice    	    { quality = 1; price = 134; };        //  (Mackerel Filet/Rice)   
+	class DDR_Item_Container_Mullet_Rice      	    { quality = 1; price = 134; };        //  (Mullet Filet/Rice)     
+	class DDR_Item_Container_Ornate_Rice      	    { quality = 1; price = 134; };        //  (Ornate Filet/Rice)     
+	class DDR_Item_Container_Rabbit_Rice      	    { quality = 1; price = 134; };        //  (Rabbit/Rice)           
+	class DDR_Item_Container_Rooster_Rice     	    { quality = 1; price = 134; };        //  (Rooster/Rice)          
+	class DDR_Item_Container_Salema_Rice      	    { quality = 1; price = 134; };        //  (Salema Filet/Rice)     
+	class DDR_Item_Container_Sheep_Rice       	    { quality = 1; price = 134; };        //  (Sheep/Rice)            
+	class DDR_Item_Container_Snake_Rice      	    { quality = 1; price = 134; };        //  (Snake/Rice)            
+	class DDR_Item_Container_Tuna_Rice       	    { quality = 1; price = 134; };        //  (Tuna Filet/Rice)       
+	class DDR_Item_Container_Turtle_Rice      	    { quality = 1; price = 134; };        //  (Turtle Filet/Rice)     
+	class DDR_Item_Container_Salemaa_Rice     	    { quality = 1; price = 134; };        //  (Salema Fresh/Rice)     
+	class DDR_Item_Container_Mackerell_Rice   	    { quality = 1; price = 134; };        //  (Mackerel Fresh/Rice)   
+	class DDR_Item_Container_Tunaa_Rice       	    { quality = 1; price = 134; };        //  (Tuna Fresh/Rice)       
+	class DDR_Item_Container_CatSharkk_Rice         { quality = 1; price = 134; };        //  (Cat Shark Fresh/Rice)  
+	class DDR_Item_Container_Mullett_Rice           { quality = 1; price = 134; };        //  (Mullet Fresh/Rice)     
+	class DDR_Item_Container_Ornatee_Rice           { quality = 1; price = 134; };        //  (Ornate Fresh/Rice)     
+	class DDR_Item_Container_Turtlee_Rice           { quality = 1; price = 134; };        //  (Turtle Fresh/Rice)     
+	class DDR_Item_Container_Chicken_Potato         { quality = 1; price = 134; };        //  (Chicken/Potato)        
+	class DDR_Item_Container_Alsatian_Potato        { quality = 1; price = 134; };        //  (Alsatian/Potato)       
+	class DDR_Item_Container_CatShark_Potato        { quality = 1; price = 134; };        //  (Cat Shark Filet/Potato)
+	class DDR_Item_Container_Fin_Potato             { quality = 1; price = 134; };        //  (Fin/Potato)            
+	class DDR_Item_Container_Goat_Potato            { quality = 1; price = 134; };        //  (Goat/Potato)           
+	class DDR_Item_Container_Mackerel_Potato        { quality = 1; price = 134; };        //  (Mackerel Filet/Potato) 
+	class DDR_Item_Container_Mullet_Potato          { quality = 1; price = 134; };        //  (Mullet Filet/Potato)   
+	class DDR_Item_Container_Ornate_Potato          { quality = 1; price = 134; };        //  (Ornate Filet/Potato)   
+	class DDR_Item_Container_Rabbit_Potato          { quality = 1; price = 134; };        //  (Rabbit/Potato)         
+	class DDR_Item_Container_Rooster_Potato         { quality = 1; price = 134; };        //  (Rooster/Potato)        
+	class DDR_Item_Container_Salema_Potato          { quality = 1; price = 134; };        //  (Salema Filet/Potato)   
+	class DDR_Item_Container_Sheep_Potato           { quality = 1; price = 134; };        //  (Sheep/Potato)          
+	class DDR_Item_Container_Snake_Potato           { quality = 1; price = 134; };        //  (Snake/Potato)          
+	class DDR_Item_Container_Tuna_Potato            { quality = 1; price = 134; };        //  (Tuna Filet/Potato)     
+	class DDR_Item_Container_Turtle_Potato          { quality = 1; price = 134; };        //  (Turtle Filet/Potato)   
+	class DDR_Item_Container_Salemaa_Potato         { quality = 1; price = 134; };        //  (Salema Fresh/Potato)   
+	class DDR_Item_Container_Mackerell_Potato       { quality = 1; price = 134; };        //  (Mackerel Fresh/Potato) 
+	class DDR_Item_Container_Tunaa_Potato           { quality = 1; price = 134; };        //  (Tuna Fresh/Potato)     
+	class DDR_Item_Container_Mullett_Potato         { quality = 1; price = 134; };        //  (Mullet Fresh/Potato)   
+	class DDR_Item_Container_CatSharkk_Potato       { quality = 1; price = 134; };        //  (Cat Shark Fresh/Potato)
+	class DDR_Item_Container_Ornatee_Potato         { quality = 1; price = 134; };        //  (Ornate Fresh/Potato)   
+	class DDR_Item_Container_Turtlee_Potato         { quality = 1; price = 134; };        //  (Turtle Fresh/Potato)   
+
+	///////////////////////////////////////////////////////////////////////////////
+	// Medical
+	///////////////////////////////////////////////////////////////////////////////
+
+	class DDR_Item_Antibiotic                       { quality = 1; price = 500; };        //  Antibiotics            
+	class DDR_Item_Bloodbag                         { quality = 1; price = 300; };        //  Blood Bag              
+	class DDR_Item_Bandage_Dirty                    { quality = 1; price = 100; };        //  Dirty Bandage          
+	class DDR_Item_Disinfectant                     { quality = 1; price = 300; };        //  Disinfectant
+	class DDR_Item_Dressing                         { quality = 1; price = 100; };        //  Military Field Dressing
+	class DDR_Item_Firstaid                         { quality = 1; price = 300; };        //  First Aid Kit 
+	class DDR_Item_Painkillers                      { quality = 1; price = 300; };        //  Painkillers 
+	class DDR_Item_Vitamins                         { quality = 1; price = 300; };        //  Vitamins 
+	class DDR_Item_Mushrooms                        { quality = 1; price = 700; };        //  Magic Mushrooms 
+	class DDR_Item_Drugs_Cocaine                    { quality = 1; price = 1500; };       //  Cocaine pack 
+	class DDR_Item_Drugs_Weed                       { quality = 1; price = 1500; };       //  Marijuana pack 
+	class DDR_Item_Heroin                           { quality = 1; price = 2000; };       //  Heroin 
+    class DDR_Item_Injector                         { quality = 1; price = 500; };        //  Expired Injector 
+	
+	///////////////////////////////////////////////////////////////////////////////
+	// Hardware And Tools
+	///////////////////////////////////////////////////////////////////////////////	
+
+    class DDR_Item_Bucket                           { quality = 1; price = 22; };         //  Metal Bucket        
+    class DDR_Item_Battery                          { quality = 1; price = 250; };        //  Vehicle Battery     
+    class DDR_Item_Measuring_Tape                   { quality = 1; price = 34; };         //  Measuring Tape      
+    class DDR_Item_Cinder_Block                     { quality = 1; price = 154; };        //  Cinder Block        
+    class DDR_Item_Main_Rotor                       { quality = 1; price = 700; };        //  Main Rotor          
+    class DDR_Item_Tailrotor                        { quality = 1; price = 600; };        //  Tail Rotor 
+	class DDR_Item_Fishing_Net                      { quality = 1; price = 100; };        //  Fishing Net
+    class DDR_Item_Pan                              { quality = 1; price = 60; };         //  Pan                 
+    class DDR_Item_Glass                            { quality = 1; price = 300; };        //  Windshield Glass    
+    class DDR_Item_Fiberglass                       { quality = 1; price = 300; };        //  Fiberglass          
+    class DDR_Item_Engine                           { quality = 1; price = 700; };        //  Engine              
+    class DDR_Item_Electrical_Component             { quality = 1; price = 154; };        //  Electrical Component
+    class DDR_Item_Old_Pot                          { quality = 1; price = 80; };         //  Old Pot             
+    class DDR_Item_Pickaxe                          { quality = 1; price = 700; };        //  Pickaxe             
+    class DDR_Item_Craft_Box                        { quality = 1; price = 500; };        //  Vehicle Box  
+	class DDR_Item_Wood_Sticks						{ quality = 1; price = 250; };        //  Wood Sticks
+	class DDR_Item_Leaves							{ quality = 1; price = 40; };         //  Leaves
+	class DDR_Item_Stones							{ quality = 1; price = 40; };	      //  Stones
+	class DDR_Item_Fuel_Tank						{ quality = 1; price = 350; };	      //  Tank
+	
+	///////////////////////////////////////////////////////////////////////////////
+	// Gemstones
+	///////////////////////////////////////////////////////////////////////////////	
+	
+    class DDR_Item_Crystal_Purple                   { quality = 1; price = 1500; };        //  Purple Crystal  
+    class DDR_Item_Crystal_Green                    { quality = 1; price = 1500; };        //  Green Crystal   
+    class DDR_Item_Gemstone_Red                     { quality = 1; price = 2000; };        //  Red Gemstone    
+    class DDR_Item_Gemstone_Blue                    { quality = 1; price = 2000; };        //  Blue Gemstone   
+    class DDR_Item_Diamond_Purple                   { quality = 1; price = 2500; };        //  Purple Diamond  
+    class DDR_Item_Diamond_Green                    { quality = 1; price = 2500; };        //  Green Diamond   
+    class DDR_Item_Diamond_Yellow                   { quality = 1; price = 3000; };        //  Yellow Diamond  
+    class DDR_Item_Diamond_Sky_Blue                 { quality = 1; price = 3000; };        //  Sky Blue Diamond
+    class DDR_Item_Diamond_Red                      { quality = 1; price = 5000; };        //  Red Diamond     
+    class DDR_Item_Diamond_Blue                     { quality = 1; price = 4000; };        //  Blue Diamond    
+    // class DDR_Item_Silver_Bar                       { quality = 1; price = 500; };         //  Silver Bar
+    // class DDR_Item_Gold_Bar                         { quality = 1; price = 1000; };        //  Gold Bar  
+    class DDR_Item_Silver_Ore                       { quality = 1; price = 250; };         //  Silver Ore
+    class DDR_Item_Gold_Ore                         { quality = 1; price = 500; };         //  Gold Ore 
+	class DDR_Item_Iron_Ore                         { quality = 1; price = 200; };         //  Iron Ore 	
