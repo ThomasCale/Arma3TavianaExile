1e8ca01428158ede85ae92aecd92db01348f4601
Thomas Cale <chris_james_ansell@hotmail.com>
2020-04-13 02:27:18 +1000

    Update autostart.bat

--- autostart.bat
+++ autostart.bat
@@ -5,7 +5,7 @@ title Server Monitor
 echo Launching Server 
 cd "C:\Arma3TavianaExile\"
 echo Server Monitor... Active !
-start "Arma3" /min /wait arma3server.exe -mod=@ExileMod;@CUPTerrainscore;@CUPTerrainsmaps;@CUPTerrainsmaps2;@ryanzombies;@RZInfection;@CBA_A3;@Arma3Taviana;@RHSAFRF;@RHSUSAF; -servermod=@ExileServer;@infiSTAR_Exile; -config=C:\Arma3TavianaExile\@ExileServer\config.cfg -port=2302 -profiles=Exile-Boss -cfg=C:\Arma3TavianaExile\@ExileServer\basic.cfg -name=anomalygamingtest -autoinit
+start "Arma3" /min /wait arma3server.exe -mod=@ExileMod;@CUPTerrainscore;@CUPTerrainsmaps;@CUPTerrainsmaps2;@EnhancedMovement;@ryanzombies;@RZInfection;@CBA_A3;@Arma3Taviana;@RHSAFRF;@RHSUSAF; -servermod=@ExileServer;@infiSTAR_Exile; -config=C:\Arma3TavianaExile\@ExileServer\config.cfg -port=2302 -profiles=Exile-Boss -cfg=C:\Arma3TavianaExile\@ExileServer\basic.cfg -name=anomalygamingtest -autoinit
 ping 127.0.0.1 -n 15 >NUL
 echo Server Shutdown ... Restarting!
 ping 127.0.0.1 -n 5 >NUL
